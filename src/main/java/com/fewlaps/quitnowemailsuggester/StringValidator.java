0c4e326997c3342b7d9b9ae12c1c53de9fe54b8f
Ed George <esm@hotmail.co.uk>
2015-09-10 17:12:41 +0100

    Refactor StringValidator
    Improve readability of isNumeric() and isAlphanumeric().

--- src/main/java/com/fewlaps/quitnowemailsuggester/StringValidator.java
+++ src/main/java/com/fewlaps/quitnowemailsuggester/StringValidator.java
@@ -5,10 +5,7 @@ public class StringValidator {
      * Only contains numbers
      */
     public boolean isNumeric(String text) {
-        if (text.matches("[0-9]+")) {
-            return true;
-        }
-        return false;
+        return text.matches("[0-9]+");
     }
 
     /**
@@ -16,9 +13,9 @@ public class StringValidator {
      * Thanks @Jacob! http://stackoverflow.com/questions/12831719/fastest-way-to-check-a-string-is-alphanumeric-in-java
      */
     public boolean isAlphanumeric(String text) {
-        for (int i = 0; i < text.length(); i++) {
+        for (int i=0; i < text.length(); i++) {
             char c = text.charAt(i);
-            if (c < 0x30 || (c >= 0x3a && c <= 0x40) || (c > 0x5a && c <= 0x60) || c > 0x7a)
+            if (!Character.isDigit(c) && !Character.isLetter(c))
                 return false;
         }
         return true;
